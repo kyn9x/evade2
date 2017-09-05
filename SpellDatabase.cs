83acadf0d5b13eae1935586ba20598f2706d1628
Kyn9x <h.kyn9x@gmail.com>
2017-09-05 14:38:30 +0700

    Update SpellDatabase.cs

--- SpellDatabase.cs
+++ SpellDatabase.cs
@@ -11,6 +11,9 @@ pass: NWNhYjVjMmE0MjVjMjllODFmYmU4NTUwMDkwNjdmN2I=
 
 user: c29sb3Rhbmt0YW5r 
 pass: YzRkZTg4OGFhZGUxNzMwNDQyOTE5ZjBkMTE0YWI3MWY=
+
+user: dXNlcmJvbHZpcA==
+pass: MmMxOTY5MzM0Y2Q1MGY2MzdiYTgzODBiOTFkOTUzYmU=
     
 fix inj: 
 
